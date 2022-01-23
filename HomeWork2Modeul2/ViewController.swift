983fa7112aae818f13c4c56926ca00393a8c311a
IlyaRakov97 <balonka97@mail.ru>
2022-01-23 11:30:45 +0400

    HomeWork

--- HomeWork2Modeul2/ViewController.swift
+++ HomeWork2Modeul2/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var colorCheningScreenView: UIView!
+    
+    @IBOutlet weak var redNumbersLabel: UILabel!
+    @IBOutlet weak var greenNumbersLabel: UILabel!
+    @IBOutlet weak var blueNumbersLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func redPlateSlider() {
+    }
+    @IBAction func greenPlateSlider() {
+    }
+    @IBAction func bluePlateSlider() {
+    }
+    
 
 }
 
