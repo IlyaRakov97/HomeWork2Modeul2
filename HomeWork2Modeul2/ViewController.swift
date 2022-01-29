701a0779cd05eee897808232dc180dfd4e1e3b9e
IlyaRakov97 <balonka97@mail.ru>
2022-01-29 10:44:09 +0400

    Constrains

--- HomeWork2Modeul2/ViewController.swift
+++ HomeWork2Modeul2/ViewController.swift
@@ -10,21 +10,63 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var colorCheningScreenView: UIView!
     
+    @IBOutlet weak var redPlateOutletSlider: UISlider!
+    @IBOutlet weak var greenPlateOutletSlider: UISlider!
+    @IBOutlet weak var bluePlateOutletSlider: UISlider!
+    
+    
     @IBOutlet weak var redNumbersLabel: UILabel!
     @IBOutlet weak var greenNumbersLabel: UILabel!
     @IBOutlet weak var blueNumbersLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    @IBAction func redPlateSlider() {
-    }
-    @IBAction func greenPlateSlider() {
-    }
-    @IBAction func bluePlateSlider() {
+        
+        colorCheningScreenView.layer.cornerRadius = 20
+        
+        colorCheningScreenView.backgroundColor = UIColor(
+            red: CGFloat(redPlateOutletSlider.value),
+            green: CGFloat(greenPlateOutletSlider.value),
+            blue: CGFloat(bluePlateOutletSlider.value),
+            alpha: 1
+        )
+        
+        redNumbersLabel.text = String(format: "%.2f",
+            redPlateOutletSlider.value)
+        greenNumbersLabel.text = String(format: "%.2f", greenPlateOutletSlider.value)
+        blueNumbersLabel.text = String(format: "%.2f", bluePlateOutletSlider.value)
+        
+        
+        redNumbersLabel.text = String(redPlateOutletSlider.value)
+        greenNumbersLabel.text = String(greenPlateOutletSlider.value)
+        blueNumbersLabel.text = String(bluePlateOutletSlider.value)
+        
+        
+        
     }
     
-
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        setColor()
+    
+        
+        redNumbersLabel.text = String(format: "%.2f",
+            redPlateOutletSlider.value)
+        greenNumbersLabel.text = String(format: "%.2f", greenPlateOutletSlider.value)
+        blueNumbersLabel.text = String(format: "%.2f", bluePlateOutletSlider.value)
+        
+    }
+    
+    private func setColor (){
+        colorCheningScreenView.backgroundColor = UIColor(
+            red: CGFloat(redPlateOutletSlider.value),
+            green: CGFloat(greenPlateOutletSlider.value),
+            blue: CGFloat(bluePlateOutletSlider.value),
+            alpha: 1
+            )
+    }
+    
+    private func string(from slider: UISlider) -> String{
+        String(format: "%.2f", slider.value)
 }
 
+}
